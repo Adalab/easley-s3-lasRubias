0b9e991ba5ec098e8099844d83037dcedb8e93bf
maria-badanina <maria.badanina.s@gmail.com>
2019-01-23 20:56:09 +0100

    Crate docs folder

---
+++ docs/precache-manifest.aad0fba728cb1664e417bbec089190fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "01830d0be699475f71409f59ee60fb09",
+    "url": "./static/media/undefined-logo.01830d0b.svg"
+  },
+  {
+    "revision": "471b278da4c31b13e10f07ce7a1613a4",
+    "url": "./static/media/logo-adalab.471b278d.svg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/descarga.0118e311.svg"
+  },
+  {
+    "revision": "2a640627eec733de7853b9e5c6616dab",
+    "url": "./static/media/default_picture.2a640627.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ca1d1f9914f0da2d4041",
+    "url": "./static/js/main.ca1d1f99.chunk.js"
+  },
+  {
+    "revision": "ea22ef1d0dcca878a172",
+    "url": "./static/js/1.ea22ef1d.chunk.js"
+  },
+  {
+    "revision": "ca1d1f9914f0da2d4041",
+    "url": "./static/css/main.0b94c4c5.chunk.css"
+  },
+  {
+    "revision": "0a9dcbd5d75bbed35dd80a29d74b5914",
+    "url": "./index.html"
+  }
+];
