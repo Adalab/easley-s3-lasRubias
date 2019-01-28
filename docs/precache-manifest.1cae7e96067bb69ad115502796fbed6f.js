04a00eca2fa1bc48cd873fc92f871f0f4b31384e
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2019-01-28 18:22:30 +0100

    docs

---
+++ docs/precache-manifest.1cae7e96067bb69ad115502796fbed6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfdd70dac8d9792cff12beabf5aa12ff",
+    "url": "./static/media/undefined-logo.bfdd70da.svg"
+  },
+  {
+    "revision": "471b278da4c31b13e10f07ce7a1613a4",
+    "url": "./static/media/logo-adalab.471b278d.svg"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/descarga.0118e311.svg"
+  },
+  {
+    "revision": "2a640627eec733de7853b9e5c6616dab",
+    "url": "./static/media/default_picture.2a640627.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "440c2a20143815a78e25",
+    "url": "./static/js/main.440c2a20.chunk.js"
+  },
+  {
+    "revision": "ea22ef1d0dcca878a172",
+    "url": "./static/js/1.ea22ef1d.chunk.js"
+  },
+  {
+    "revision": "440c2a20143815a78e25",
+    "url": "./static/css/main.d0c428ca.chunk.css"
+  },
+  {
+    "revision": "80ce228f54b5f9a9e1ec96081da0148a",
+    "url": "./index.html"
+  }
+];
